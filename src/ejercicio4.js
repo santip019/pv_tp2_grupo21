0f7e60549d53fdde4e7f7abf699340684d2b4a12
Antonella Guadalupe Cruz <antoguadalupecruz@gmail.com>
2025-04-12 14:09:59 -0300

    Ejercicio 4 realizado por Cruz Antonella Guadalupe

--- src/ejercicio4.js
+++ src/ejercicio4.js
@@ -1,6 +1,7 @@
-let l=5
-let area=l*l
-let perimetro=l*4
-Console.log("El lado del cuadrado es: " + l)
-console.log("El area del cuadrado es: " + area)
-console.log("El perimetro del cuadrado es: " + perimetro)
+let lado=5;
+let area=lado*lado;
+let perimetro=lado*4;
+
+console.log("El lado del cuadrado es: " + lado);
+console.log("El area del cuadrado es: " + area);
+console.log("El perimetro del cuadrado es: " + perimetro);
