e63c969c5ed0906d514b175136df8c9fc4246e36
maxiicalpa <maxiicalpa1gmail.com>
2025-04-12 00:55:46 -0300

    ejercicio 1 realiza por Calpanchay Maximiliano

---
+++ js/ejercicio1.js
@@ -0,0 +1,3 @@
+let integrantes
+integrantes = "Calpanchay Maximiliano, Cruz Antonella, Fernandez Bruno, Padila Santiago, Valentina Efimov"
+console.log("Somos " + integrantes + " y estamos aprendiendo JavaScript.")
