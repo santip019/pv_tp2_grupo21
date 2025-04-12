3ffa95a9fb6db64acc5545282024d037b3b2136e
Antonella Guadalupe Cruz <antoguadalupecruz@gmail.com>
2025-04-12 13:27:08 -0300

    Estructuracion de carpetas

--- src/ejercicio4.js
+++ src/ejercicio4.js
@@ -1,5 +1,6 @@
 let lado=5
 let area=lado*lado
 let perimetro=lado*4
+Console.log("El lado del cuadrado es: " + lado)
 console.log("El area del cuadrado es: " + area)
 console.log("El perimetro del cuadrado es: " + perimetro)
---
+++ src/ejercicio5.js
@@ -0,0 +1,9 @@
+let horas=10
+let minutos=50
+
+let segundos
+segundos = horas * 3600 + minutos * 60
+
+console.log("Horas: " + horas)
+console.log("Minutos: " + minutos)
+console.log("Conversion en segundos: " + segundos)
