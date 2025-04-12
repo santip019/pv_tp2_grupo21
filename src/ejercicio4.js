be241bfe5968e43ba06b7301cc0a4b0a9eb37d85
Antonella Guadalupe Cruz <antoguadalupecruz@gmail.com>
2025-04-12 13:31:48 -0300

    Punto 4 realizado por Cruz Antonella Guadalupe

--- src/ejercicio4.js
+++ src/ejercicio4.js
@@ -1,6 +1,6 @@
-let lado=5
-let area=lado*lado
-let perimetro=lado*4
-Console.log("El lado del cuadrado es: " + lado)
+let l=5
+let area=l*l
+let perimetro=l*4
+Console.log("El lado del cuadrado es: " + l)
 console.log("El area del cuadrado es: " + area)
 console.log("El perimetro del cuadrado es: " + perimetro)
