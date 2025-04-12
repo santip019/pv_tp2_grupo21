ab6cf4a029a0e590db4c2d8cf84276120541f535
Antonella Guadalupe Cruz <antoguadalupecruz@gmail.com>
2025-04-12 14:11:41 -0300

    Ejercicio 5 realizado por Cruz Antonella Guadalupe

--- src/ejercicio5.js
+++ src/ejercicio5.js
@@ -1,9 +1,9 @@
-let horas=10
-let minutos=50
+let horas=10;
+let minutos=50;
 
-let segundos
-segundos = horas * 3600 + minutos * 60
+let segundos;
+segundos = horas * 3600 + minutos * 60;
 
-console.log("Horas: " + horas)
-console.log("Minutos: " + minutos)
-console.log("Conversion en segundos: " + segundos)
+console.log("Horas: " + horas);
+console.log("Minutos: " + minutos);
+console.log("Total de horas y minutos convertidas en segundos: " + segundos);
