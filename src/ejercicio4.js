3d0685faae84ec02cda287b8413ef4ac34022887
santip019 <smauriciop001@gmail.com>
2025-04-12 22:59:46 -0300

    Revert "Ejercicios 13, 14 y 17 realizados por Valentina Efimov"

---
+++ src/ejercicio4.js
@@ -0,0 +1,7 @@
+let lado=5;
+let area=lado*lado;
+let perimetro=lado*4;
+
+console.log("El lado del cuadrado es: " + lado);
+console.log("El area del cuadrado es: " + area);
+console.log("El perimetro del cuadrado es: " + perimetro);
---
+++ src/ejercicio5.js
@@ -0,0 +1,9 @@
+let horas=10;
+let minutos=50;
+
+let segundos;
+segundos = horas * 3600 + minutos * 60;
+
+console.log("Horas: " + horas);
+console.log("Minutos: " + minutos);
+console.log("Total de horas y minutos convertidas en segundos: " + segundos);
