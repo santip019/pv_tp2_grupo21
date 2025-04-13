5a06807acbe882cb4e11c6ce1b3cbdf207f2ab93
Valentina Efimov <valentinaefimov0811@gmail.com>
2025-04-12 22:00:48 -0300

    Ejercicio 13 resuelto por Valentina Efimov

--- src/ejercicio1.js
+++
@@ -1,3 +0,0 @@
-let integrantes
-integrantes = "Calpanchay Maximiliano, Cruz Antonella, Fernandez Bruno, Padila Santiago, Valentina Efimov"
-console.log("Somos " + integrantes + " y estamos aprendiendo JavaScript.")
