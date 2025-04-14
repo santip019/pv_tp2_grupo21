faed7d9f5529299f0a2a513f6c7a09480b292466
maxiicalpa <maxiicalpa1gmail.com>
2025-04-13 21:32:24 -0300

    Ejercicios 10, 11 y 12 corregidos por Calpanchay Maximiliano

---
+++ src/ejercicio10.js
@@ -0,0 +1,6 @@
+let num1 = parseInt(prompt("Ingrese el primer numero entero:"))
+let num2 = parseInt(prompt("Ingrese el segundo numero entero:"))
+let num3 = parseInt(prompt("Ingrese el tercer numero entero:"))
+
+let promedio = (num1 + num2 + num3) / 3
+alert("El promedio de los tres numeros es: " + promedio)
---
+++ src/ejercicio12.js
@@ -0,0 +1,8 @@
+
+let num = parseInt(prompt("Ingrese un número: "))
+if (num % 2 == 0) {
+    alert("El numero " + num + " es par")
+}
+else {
+    alert("El numero " + num + " es impar")
+}
