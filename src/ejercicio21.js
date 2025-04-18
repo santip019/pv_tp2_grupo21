b88fbff40ce42766af21d21ba8f4a7863e35e610
Antonella Guadalupe Cruz <antoguadalupecruz@gmail.com>
2025-04-17 21:14:29 -0300

    Punto 21 - Cruz Antonella Guadalupe

---
+++ src/ejercicio21.js
@@ -0,0 +1,6 @@
+import { actualizarTexto } from "/src/actualizartexto21.js";
+
+const input = document.getElementById("inputTexto");
+const textoMostrado = document.getElementById("textoMostrado");
+
+actualizarTexto(input, textoMostrado);
